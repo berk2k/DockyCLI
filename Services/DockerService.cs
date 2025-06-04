d7ae439817fcbf6a061bf0c65a58a77b0767b86e
Berk Polat <berkpolat125@gmail.com>
2025-06-04 14:07:56 +0200

    docker service added

---
+++ Services/DockerService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockyCLI.Services
+{
+    public class DockerService : IDockerService
+    {
+        public (string output, string error) RunDockerCommand(string arguments)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "docker",
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+
+            return (output, error);
+        }
+    }
+}
