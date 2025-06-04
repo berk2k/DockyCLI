91e92087fb189dfb0dded3be08642e6d09e1a1ef
Berk Polat <berkpolat125@gmail.com>
2025-06-04 14:57:13 +0200

    docker service updated and parser method added

--- Services/DockerService.cs
+++ Services/DockerService.cs
@@ -1,22 +1,24 @@
-﻿using System;
+﻿using DockyCLI.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DockyCLI.Services
 {
     public class DockerService : IDockerService
     {
-        public (string output, string error) RunDockerCommand(string arguments)
+        public List<ContainerInfo> GetRunningContainers()
         {
             var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "docker",
-                    Arguments = arguments,
+                    Arguments = "ps",
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     UseShellExecute = false,
@@ -26,12 +28,44 @@ namespace DockyCLI.Services
 
             process.Start();
 
-            string output = process.StandardOutput.ReadToEnd();
-            string error = process.StandardError.ReadToEnd();
+            var output = process.StandardOutput.ReadToEnd();
+            var error = process.StandardError.ReadToEnd();
 
             process.WaitForExit();
 
-            return (output, error);
+            if (!string.IsNullOrWhiteSpace(error))
+                throw new Exception($"Docker error: {error}");
+
+            return ParseDockerPsOutput(output);
+        }
+
+        private List<ContainerInfo> ParseDockerPsOutput(string output)
+        {
+            var lines = output.Split('\n',StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length < 2)
+                return new List<ContainerInfo>();
+
+            var containers = new List<ContainerInfo>();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var parts = Regex.Split(line.Trim(), @"\s{2,}");
+                if (parts.Length < 7)
+                    continue;
+
+                containers.Add(new ContainerInfo
+                {
+                    ContainerId = parts[0],
+                    Image = parts[1],
+                    Command = parts[2],
+                    Created = parts[3],
+                    Status = parts[4],
+                    Ports = parts[5],
+                    Names = parts[6]
+                });
+            }
+
+            return containers;
         }
     }
 }
--- Services/IDockerService.cs
+++ Services/IDockerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DockyCLI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace DockyCLI.Services
 {
     public interface IDockerService
     {
-        (string output, string error) RunDockerCommand(string arguments);
+        List<ContainerInfo> GetRunningContainers();
     }
 }
