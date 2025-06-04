a2a7b8adcf9aa24159b57ee5b2cd4b0fcf929c41
Berk Polat <berkpolat125@gmail.com>
2025-06-04 14:51:45 +0200

    container info model added

---
+++ Models/ContainerInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockyCLI.Models
+{
+    public class ContainerInfo
+    {   
+        public string ContainerId { get; set; } = "";
+        public string Image { get; set; } = "";
+        public string Command { get; set; } = "";
+        public string Created { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Ports { get; set; } = "";
+        public string Names { get; set; } = "";
+    }
+}
