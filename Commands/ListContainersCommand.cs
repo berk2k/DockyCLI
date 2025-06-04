55e48adea576cc71b5df9e997d983714a5951067
Berk Polat <berkpolat125@gmail.com>
2025-06-04 14:05:15 +0200

    List container commands added

---
+++ Commands/ListContainersCommand.cs
@@ -0,0 +1,55 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockyCLI.Commands
+{
+    public class ListContainersCommand : Command<ListContainersCommand.Settings>
+    {
+        public override int Execute(CommandContext context, Settings settings)
+        {
+            try
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "docker",
+                        Arguments = "ps",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrWhiteSpace(error))
+                {
+                    AnsiConsole.MarkupLine($"[red]Error:[/] {error}");
+                    return -1;
+                }
+                AnsiConsole.MarkupLine("[green]Active Docker containers:[/]");
+                AnsiConsole.WriteLine(output);
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                AnsiConsole.MarkupLine($"[red]Exception:[/] {ex.Message}");
+                return -1;
+            }
+        }
+
+        public class Settings : CommandSettings { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Spectre.Console.Cli;
+
+using System;
+using System.ComponentModel;
+
+var app = new CommandApp();
+
+app.Configure(config =>
+{
+    config.SetApplicationName("docky");
+    config.AddCommand<DockyCLI.Commands.ListContainersCommand>("list")
+        .WithDescription("Listing Docker Containers");
+});
+
+return app.Run(args);
+
