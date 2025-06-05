f76142636448192a2ee88aa7f7b4393743946277
Berk Polat <berkpolat125@gmail.com>
2025-06-05 14:32:16 +0200

    list image command added

---
+++ Commands/ListImagesCommand.cs
@@ -0,0 +1,48 @@
+﻿using DockyCLI.Presantation;
+using DockyCLI.Services;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockyCLI.Commands
+{
+    public class ListImagesCommand : Command<ListImagesCommand.Settings>
+    {
+        private readonly IDockerService _dockerService;
+        private readonly OutputRenderer _renderer;
+
+        public ListImagesCommand(IDockerService dockerService, OutputRenderer renderer)
+        {
+            _dockerService = dockerService;
+            _renderer = renderer;
+        }
+
+        public class Settings : CommandSettings { }
+
+        public override int Execute(CommandContext context, Settings settings)
+        {
+            try
+            {
+                var images = _dockerService.GetImages();
+
+                if (images.Count == 0)
+                {
+                    AnsiConsole.MarkupLine("[yellow]No Docker images found.[/]");
+                    return 0;
+                }
+
+                _renderer.RenderImageTable(images);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                AnsiConsole.MarkupLine($"[red]Error:[/] {ex.Message}");
+                return -1;
+            }
+        }
+    }
+}
