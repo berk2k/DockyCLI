0fdb83cb7c1fa5901a91490ff23ea20012c5da72
Berk Polat <berkpolat125@gmail.com>
2025-06-08 11:43:40 +0200

    start container added

--- Docky.Desktop/ViewModels/MainViewModel.cs
+++ Docky.Desktop/ViewModels/MainViewModel.cs
@@ -14,12 +14,14 @@ namespace Docky.Desktop.ViewModels
         public ObservableCollection<ContainerInfo> Containers { get; set; }
 
         public ICommand StopContainerCommand { get; }
+        public ICommand StartContainerCommand { get; }
 
         public MainViewModel(IDockerService dockerService)
         {
             _dockerService = dockerService;
             Containers = new ObservableCollection<ContainerInfo>();
             StopContainerCommand = new RelayCommand(StopContainer);
+            StartContainerCommand = new RelayCommand(StartContainer);
 
             LoadContainers();
         }
@@ -45,6 +47,20 @@ namespace Docky.Desktop.ViewModels
                 System.Windows.MessageBox.Show($"Container stopped: {container.ContainerId}", "Success");
             else
                 System.Windows.MessageBox.Show($"Failed to stop container: {container.ContainerId}", "Error");
+
+            ReloadContainers();
+        }
+
+        private void StartContainer(object? parameter)
+        {
+            if (parameter is not ContainerInfo container)
+                return;
+            (bool Success, string Output, string Error) = _dockerService.StartContainer(container.ContainerId);
+
+            if (Success)
+                System.Windows.MessageBox.Show($"Container started: {container.ContainerId}", "Success");
+            else
+                System.Windows.MessageBox.Show($"Failed to start container: {container.ContainerId}", "Error");
 
             ReloadContainers();
         }
